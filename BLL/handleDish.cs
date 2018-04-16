31139940c73d2009a1bfdec8efab6a5d511dd9b6
钟正 <616028858@qq.com>
2018-04-16 14:20:18 +0800

    添加菜单的逻辑类；

---
+++ BLL/handleDish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class handleDish
+    {
+        //获取所有菜单
+        public DataTable GetDishList()
+        {
+            string str = @"select     id,
+                                      CONVERT(varchar(19), create_time, 120) as create_time
+                               from dbo.ls_dish";
+            str = string.Format(str);
+            DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
+
+            return dt;
+        }
+    }
+}
