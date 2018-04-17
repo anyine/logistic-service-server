5a4a493a3c142b33f042f247141bf645c0646467
zhongzhenga <616028858@qq.com>
2018-04-18 00:06:00 +0800

    修改菜品查询逻辑；

--- BLL/handleDish.cs
+++ BLL/handleDish.cs
@@ -18,15 +18,37 @@ namespace BLL
                                     companyId,
                                     dish_type,
                                     ISNULL(is_online, 0) as is_online,
+                                    CONVERT(varchar(19), update_time, 120) as update_time,
                                     CONVERT(varchar(19), create_time, 120) as create_time
-                               from dbo.ls_dish";
+                               from dbo.ls_dish
+                               order by create_time desc";
             str = string.Format(str);
             DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
 
             return dt;
         }
 
-        //新增或修改菜品
+        //获取菜单详情
+        public DataTable GetDishDetail(string id)
+        {
+            string str = @"select   id,
+                                    dish_title,
+                                    dish_content,
+                                    dish_img,
+                                    companyId,
+                                    dish_type,
+                                    ISNULL(is_online, 0) as is_online,
+                                    CONVERT(varchar(19), update_time, 120) as update_time,
+                                    CONVERT(varchar(19), create_time, 120) as create_time
+                               from dbo.ls_dish
+                               where id='{0}'";
+            str = string.Format(str, id);
+            DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
+
+            return dt;
+        }
+
+        //新增菜品
         public bool AddDish(string dish_title, string dish_content, string dish_img, string companyId, string dish_type)
         {
             string str = @"insert into dbo.ls_dish (dish_title, dish_content, dish_img, companyId, dish_type)
@@ -38,11 +60,23 @@ namespace BLL
             return flag > 0 ? true : false;
         }
 
+        //修改菜品信息
+        public bool EditDish(string id, string dish_title, string dish_content, string dish_img, string companyId, string dish_type)
+        {
+            string str = @"update dbo.ls_dish set dish_title='{1}', dish_content='{2}', dish_img='{3}', companyId='{4}', dish_type='{5}' where id='{0}'";
+            str = string.Format(str, id, dish_title, dish_content, dish_img, companyId, dish_type);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
         //更新菜品推荐状态
         public bool OnlineStateChange(string id, int onlineState)
         {
-            string str = @"update dbo.ls_dish set is_online={1} where id='{0}'";
-            str = string.Format(str, id, onlineState);
+            DateTime dt = DateTime.Now;
+
+            string str = @"update dbo.ls_dish set is_online={1}, update_time='{2}' where id='{0}'";
+            str = string.Format(str, id, onlineState, dt);
             int flag = DBHelper.SqlHelper.ExecuteSql(str);
 
             return flag > 0 ? true : false;
