b73c24c1a6c10b1fc3890663b4d1cc07f31237bb
钟正 <616028858@qq.com>
2018-04-17 13:54:02 +0800

    新增修改菜品状态方法；

--- BLL/handleDish.cs
+++ BLL/handleDish.cs
@@ -11,8 +11,14 @@ namespace BLL
         //获取所有菜单
         public DataTable GetDishList()
         {
-            string str = @"select     id,
-                                      CONVERT(varchar(19), create_time, 120) as create_time
+            string str = @"select   id,
+                                    dish_title,
+                                    dish_content,
+                                    dish_img,
+                                    companyId,
+                                    dish_type,
+                                    ISNULL(is_online, 0) as is_online,
+                                    CONVERT(varchar(19), create_time, 120) as create_time
                                from dbo.ls_dish";
             str = string.Format(str);
             DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
@@ -27,6 +33,16 @@ namespace BLL
                                 values ('{0}', '{1}', '{2}', '{3}', '{4}')";
             str = string.Format(str, dish_title, dish_content, dish_img, companyId, dish_type);
             CommonTool.WriteLog.Write("AddDish str === " + str);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
+        //更新菜品推荐状态
+        public bool OnlineStateChange(string id, int onlineState)
+        {
+            string str = @"update dbo.ls_dish set is_online={1} where id='{0}'";
+            str = string.Format(str, id, onlineState);
             int flag = DBHelper.SqlHelper.ExecuteSql(str);
 
             return flag > 0 ? true : false;
