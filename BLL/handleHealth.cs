706d00ff8aa2681b388ce655a5226062278aea5b
zhongzhenga <616028858@qq.com>
2018-05-07 16:38:42 +0800

    新增健康模型；

---
+++ BLL/handleHealth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class handleHealth
+    {
+        //获取所有健康信息
+        public DataTable GetHealthList()
+        {
+            string str = @"select   id,
+                                    companyId,
+                                    health_cover,
+                                    health_title,
+                                    health_desc,
+                                    health_content,
+                                    CONVERT(varchar(19), create_time, 120) as create_time
+                               from dbo.ls_health
+                               order by create_time desc";
+            str = string.Format(str);
+            DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
+
+            return dt;
+        }
+
+        //新增健康信息                                
+        public bool AddHealth(string companyId, string health_cover, string health_title, string health_desc, string health_content)
+        {
+            string str = @"insert into dbo.ls_health (companyId, health_cover, health_title, health_desc, health_content)
+                                values ('{0}', '{1}', '{2}', '{3}', '{4}')";
+            str = string.Format(str, companyId, health_cover, health_title, health_desc, health_content);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+    }
+}
---
+++ logistic-service-server/Models/health.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace logistic_service_server.Models
+{
+    public class health
+    {
+        public string id { set; get; }
+        public string companyId { set; get; }
+        public string health_cover { set; get; }
+        public string health_title { set; get; }
+        public string health_desc { set; get; }
+        public string health_content { set; get; }
+        public string create_time { set; get; }
+    }
+}
