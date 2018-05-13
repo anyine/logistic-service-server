7c02cf636e02609d79448680814687f44153f234
zhongzhenga <616028858@qq.com>
2018-05-13 17:19:11 +0800

    修改逻辑；

--- BLL/handleCompany.cs
+++ BLL/handleCompany.cs
@@ -34,7 +34,7 @@ namespace BLL
                                     service_content,
                                     service_cover,
                                     CONVERT(varchar(19), create_time, 120) as create_time
-                               from dbo.ls_serivce
+                               from dbo.ls_service
                                order by create_time desc";
             str = string.Format(str);
             DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
@@ -79,6 +79,45 @@ namespace BLL
             int flag = DBHelper.SqlHelper.ExecuteSql(str);
 
             return flag > 0 ? true : false;
+        }
+
+        //新增服务
+        public bool AddService(string companyId, string service_type, string service_title, string service_content)
+        {
+            string str = @"insert into dbo.ls_service (companyId, service_type, service_title,  service_content)
+                                values ('{0}', '{1}', '{2}', '{3}')";
+            str = string.Format(str, companyId, service_type, service_title, service_content);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
+        //修改服务信息
+        public bool EditService(string id, string companyId, string service_type, string service_title, string service_content)
+        {
+            string str = @"update dbo.ls_service set companyId='{1}', service_type='{2}', service_title='{3}', service_content='{4}' where id='{0}'";
+            str = string.Format(str, id, companyId, service_type, service_title, service_content);
+            CommonTool.WriteLog.Write("str == " + str);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
+        //获取服务详情
+        public DataTable GetServiceDetail(string id)
+        {
+            string str = @"select   id,
+                                    companyId,
+                                    service_type,
+                                    service_title,
+                                    service_content,
+                                    CONVERT(varchar(19), create_time, 120) as create_time
+                               from dbo.ls_service
+                               where id='{0}'";
+            str = string.Format(str, id);
+            DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
+
+            return dt;
         }
     }
 }
--- BLL/handleSurvey.cs
+++ BLL/handleSurvey.cs
@@ -14,10 +14,11 @@ namespace BLL
             string str = @"select   id,
                                     companyId,
                                     satisfaction,
-                                    telephone,
-                                    suggestion,
+                                    dish,
+                                    clean,
                                     CONVERT(varchar(19), create_time, 120) as create_time
                                from dbo.ls_survey
+                               where datediff(month, create_time, getdate())=0
                                order by create_time desc";
             str = string.Format(str);
             DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
@@ -26,11 +27,22 @@ namespace BLL
         }
 
         //新增调查问卷
-        public bool AddSurvey(string companyId, string satisfaction, string telephone, string suggestion)
+        public bool AddSurvey(string companyId, string satisfaction, string dish, string clean)
         {
-            string str = @"insert into dbo.ls_survey (companyId, satisfaction, telephone, suggestion)
+            string str = @"insert into dbo.ls_survey (companyId, satisfaction, dish, clean)
                                 values ('{0}', '{1}', '{2}', '{3}')";
-            str = string.Format(str, companyId, satisfaction, telephone, suggestion);
+            str = string.Format(str, companyId, satisfaction, dish, clean);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
+        //新增需求
+        public bool AddNeed(string companyId, string telephone, string suggestion)
+        {
+            string str = @"insert into dbo.ls_need (companyId, telephone, suggestion)
+                                values ('{0}', '{1}', '{2}')";
+            str = string.Format(str, companyId, telephone, suggestion);
             int flag = DBHelper.SqlHelper.ExecuteSql(str);
 
             return flag > 0 ? true : false;
