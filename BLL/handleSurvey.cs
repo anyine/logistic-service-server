1d0b23c774a440a6759a59529b5060baafe3996d
钟正 <616028858@qq.com>
2018-06-13 15:06:47 +0800

    新增运动场馆api；

--- BLL/handleSurvey.cs
+++ BLL/handleSurvey.cs
@@ -48,5 +48,60 @@ namespace BLL
             return flag > 0 ? true : false;
         }
 
+        //获取所有球场信息
+        public DataTable GetGymList()
+        {
+            string str = @"select   id,
+                                    ball_type,
+                                    ball_content,
+                                    CONVERT(varchar(19), create_time, 120) as create_time
+                               from dbo.ls_ball
+                               order by create_time desc";
+            str = string.Format(str);
+            DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
+
+            return dt;
+        }
+
+        //新增运动
+        public bool AddBall(string ball_type, string ball_content)
+        {
+            string str = @"insert into dbo.ls_ball (ball_type,  ball_content)
+                                values ('{0}', '{1}')";
+            str = string.Format(str, ball_type, ball_content);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
+        //修改运动信息
+        public bool EditBall(string id, string ball_type, string ball_content)
+        {
+            string str = @"update dbo.ls_ball set ball_type='{1}', ball_content='{2}' where id='{0}'";
+            str = string.Format(str, id, ball_type, ball_content);
+            CommonTool.WriteLog.Write("str == " + str);
+            int flag = DBHelper.SqlHelper.ExecuteSql(str);
+
+            return flag > 0 ? true : false;
+        }
+
+        //获取运动详情
+        public DataTable GetBallDetail(string id)
+        {
+            string str = @"select   id,
+                                    companyId,
+                                    service_type,
+                                    service_title,
+                                    service_cover,
+                                    service_content,
+                                    CONVERT(varchar(19), create_time, 120) as create_time
+                               from dbo.ls_service
+                               where id='{0}'";
+            str = string.Format(str, id);
+            DataTable dt = DBHelper.SqlHelper.GetDataTable(str);
+
+            return dt;
+        }
+
     }
 }
---
+++ logistic-service-server/Models/ball.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace logistic_service_server.Models
+{
+    public class ball
+    {
+        public string id { set; get; }
+        public string ball_type { set; get; }
+        public string ball_content { set; get; }
+        public string create_time { set; get; }
+    }
+}
